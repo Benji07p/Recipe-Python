fcb8406081bb8308c00933bd098535ef135fae59
Benji07p <82090430+Benji07p@users.noreply.github.com>
2021-04-08 14:19:48 +0200

    Add files via upload

---
+++ Recipe.py
@@ -0,0 +1,12 @@
+print ("Combien de personnes ?")
+print ("taper n = (nombres de personnes)")
+n = 3
+oeufs = n*3//4
+chocolat = n*100//4
+sucre_vanille = n*1/4
+print ("Nombres d'oeufs :")
+print (oeufs)
+print ("Grammes de chocolats :")
+print (chocolat)
+print ("Nombres de sachet de sucre vanillés :")
+print (sucre_vanille)
